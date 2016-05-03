50db5c0a7d7039c2232c79d4bbff10539ecfa861
halfking <kimmy.huang@gmail.com>
2016-05-04 00:09:43 +0800

    seconds commits

---
+++ HCPlayerTest/HCPlayerTest/UIPlayerTestVC.h
@@ -0,0 +1,13 @@
+//
+//  UIPlayerTestVC.h
+//  HCPlayerTest
+//
+//  Created by HUANGXUTAO on 16/5/3.
+//  Copyright © 2016年 seenvoice.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIPlayerTestVC : UIViewController
+
+@end
---
+++ HCPlayerTest/HCPlayerTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCPlayerTest
+//
+//  Created by HUANGXUTAO on 16/5/3.
+//  Copyright © 2016年 seenvoice.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
