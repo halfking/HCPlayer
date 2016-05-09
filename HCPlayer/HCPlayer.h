4ce078fc9f1c5a878d6fd914805a889d7f7503ac
halfking <kimmy.huang@gmail.com>
2016-05-09 19:57:47 +0800

    fix bugs when release2

--- HCPlayer/HCPlayer.h
+++ HCPlayer/HCPlayer.h
@@ -38,7 +38,7 @@ FOUNDATION_EXPORT const unsigned char HCPlayerVersionString[];
 
 #import <HCPlayer/VDCLoaderConnectionN.h>
 #import <HCPlayer/VDCTempFileManagerN.h>
-#import <HCPlayer/VDCTempFileManagerN(readerwrite).h>
+#import <HCPlayer/VDCTempFileManagerN(readerwriter).h>
 
 
 //#import <HCPlayer/PlayerMediaItem.h>
